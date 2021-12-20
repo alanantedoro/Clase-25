8c2026f2063d8a40f34b4a45e19671a8fc740174
alanantedoro <alanantedoro@gmail.com>
2021-12-19 22:29:24 -0300

    Entregable 26

---
+++ src/main.js
@@ -0,0 +1,65 @@
+import express from 'express'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+
+import config from './config.js'
+
+import { Server as HttpServer } from 'http'
+import { Server as Socket } from 'socket.io'
+
+import authWebRouter from './routers/web/auth.js'
+import homeWebRouter from './routers/web/home.js'
+import productosApiRouter from './routers/api/productos.js'
+
+import addProductosHandlers from './routers/ws/productos.js'
+import addMensajesHandlers from './routers/ws/mensajes.js'
+import passport from 'passport'
+
+import mongoose from 'mongoose'
+
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new Socket(httpServer)
+
+io.on('connection', async socket => {
+    addProductosHandlers(socket, io.sockets)
+    addMensajesHandlers(socket, io.sockets)
+});
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
+
+
+app.use(session({
+    store: MongoStore.create({ mongoUrl: config.mongoLocal.cnxStr }),
+    secret: 'muyimportante',
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {
+        maxAge: 60000
+    }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(productosApiRouter)
+
+app.use(authWebRouter)
+app.use(homeWebRouter)
+
+mongoose.connect(config.mongoLocal.cnxStr, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+    if(err) {
+      console.error('Error - connection mongo');
+    }});
+
+const connectedServer = httpServer.listen(config.PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`)
+})
+connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
